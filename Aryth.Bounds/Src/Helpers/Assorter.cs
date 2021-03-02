39ce37cf5bc0a21ee31b43e30e28daa4dfbfa21e
hoyeungw <hoyeungw@outlook.com>
2021-03-03 07:42:15 +0800

    minor fix

--- Aryth.Bounds/Src/Helpers/Assorter.cs
+++ Aryth.Bounds/Src/Helpers/Assorter.cs
@@ -2,7 +2,26 @@
 using Typen.Numeral;
 
 namespace Aryth.Bounds.Helpers {
-  public static partial class Assorter {
+  public static class Assorter {
+    public static double ToF64<T>(this T o) {
+      if (o == null) return double.NaN;
+      switch (o) {
+        case double n: return n;
+        case sbyte n: return n;
+        case short n: return n;
+        case int n: return n;
+        case long n: return n;
+        case decimal n: return (double) n;
+        case float n: return n;
+        case bool n: return n ? 1 : 0;
+        case byte n: return n;
+        case ushort n: return n;
+        case uint n: return n;
+        case ulong n: return n;
+        case string s: return s.CastDouble();
+        default: return o.ToString().CastDouble();
+      }
+    }
     public static (double x, double y) Assort<T>(T x) {
       var s = x.ToString();
       return double.TryParse(s, out var n)
@@ -32,7 +51,12 @@ namespace Aryth.Bounds.Helpers {
       if (bound == null) bound = (n, n);
       else {
         var pair = bound.Value;
-        if (n > pair.max) { pair.max = n; } else if (n < pair.min) { pair.min = n; }
+        if (n > pair.max) {
+          pair.max = n;
+        }
+        else if (n < pair.min) {
+          pair.min = n;
+        }
         bound = pair;
       }
     }
