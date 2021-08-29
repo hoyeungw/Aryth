fad3d28ec03c7ac7d45c39aeb96df23603503670
hoyeungw <hoyeungw@outlook.com>
2021-08-29 18:15:47 +0800

    add convertion functions

--- Aryth.Polar/Src/Polar/Util.cs
+++ Aryth.Polar/Src/Polar/Util.cs
@@ -4,7 +4,8 @@ namespace Aryth.Polar {
   public static class Util {
     public static double DegreeToRadian(double degree) => degree * PI / 180;
     public static double RadianToDegree(double radian) => radian * 180 / PI;
-
+    public static (double r, double θ) DegreeToRadian(this (double r, double θ) polar) => (polar.r, DegreeToRadian(polar.θ));
+    public static (double r, double θ) RadianToDegree(this (double r, double θ) polar) => (polar.r, RadianToDegree(polar.θ));
     public static (double r, double θ) Rotate(this (double r, double θ) polar, double degree) {
       var θ = polar.θ + degree;
       while (θ > 360) θ -= 360;
