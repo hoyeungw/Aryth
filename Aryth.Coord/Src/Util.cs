1cd9da3e1345ae2cff99d7da9bfa7cc533f60bdb
hoyeungw <hoyeungw@outlook.com>
2021-08-29 11:31:57 +0800

    add Aryth.Coord

---
+++ Aryth.Coord/Src/Util.cs
@@ -0,0 +1,12 @@
+﻿using static System.Math;
+
+namespace Aryth.Coord {
+  public static class Util {
+    public static double Radius(this (double x, double y) coord) {
+      var (x, y) = coord;
+      return Sqrt(x * x + y * y);
+    }
+    public static (double x, double y) RoundD1(this (double x, double y) coord) =>
+      (Math.RoundD1(coord.x), Math.RoundD1(coord.y));
+  }
+}
