e13268f689a57acd8c5ae8fe55932fb5ad6f3c2a
hoyeungw <hoyeungw@outlook.com>
2021-08-29 18:15:34 +0800

    add FlopperFactory

--- Aryth.Flopper/Src/FlopperFactory.cs
+++ Aryth.Flopper/Src/FlopperFactory.cs
@@ -14,5 +14,16 @@ namespace Aryth {
       while (--hi >= 0) yield return vec.Swap(rand.Next(hi), hi);
       while (true) yield return df;
     }
+    public static IEnumerable<T> FiniteFlopper<T>(this List<T> vec) {
+      var rand = new Random();
+      var hi = vec.Count;
+      while (--hi >= 0) yield return vec.Swap(rand.Next(hi), hi);
+    }
+    public static IEnumerator<T> InfiniteFlopper<T>(this List<T> vec, T df = default) {
+      var rand = new Random();
+      var hi = vec.Count;
+      while (--hi >= 0) yield return vec.Swap(rand.Next(hi), hi);
+      while (true) yield return df;
+    }
   }
 }
--- Aryth.Flopper/Src/FlopperUtil.cs
+++ Aryth.Flopper/Src/FlopperUtil.cs
@@ -1,6 +1,13 @@
-﻿namespace Aryth {
+﻿using System.Collections.Generic;
+
+namespace Aryth {
   public static class FlopperUtil {
     public static T Swap<T>(this T[] vec, int i, int j) {
+      var temp = vec[i];
+      vec[i] = vec[j];
+      return vec[j] = temp;
+    }
+    public static T Swap<T>(this List<T> vec, int i, int j) {
       var temp = vec[i];
       vec[i] = vec[j];
       return vec[j] = temp;
