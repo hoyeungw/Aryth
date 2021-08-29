c461ad79e485be7b8b93748cc119d600d5d0e502
hoyeungw <hoyeungw@outlook.com>
2021-08-29 11:31:35 +0800

    add Aryth.Polar.Util

---
+++ Aryth.Polar/Util.cs
@@ -0,0 +1,8 @@
+﻿using static System.Math;
+
+namespace Aryth.Polar {
+  public static class Util {
+    public static double DegreeToRadian(double degree) => degree * PI / 180;
+    public static double RadianToDegree(double radian) => radian * 180 / PI;
+  }
+}
