38ad0338cb09c004bdccb703d0f529d60ea35e29
hoyeungw <hoyeungw@outlook.com>
2021-08-30 09:18:57 +0800

    add more Round functions

--- Aryth.Coord/Src/Coord/Util.cs
+++ Aryth.Coord/Src/Coord/Util.cs
@@ -6,7 +6,9 @@ namespace Aryth.Coord {
       var (x, y) = coord;
       return Sqrt(x * x + y * y);
     }
-    public static (double x, double y) RoundD1(this (double x, double y) coord) =>
-      (Math.RoundD1(coord.x), Math.RoundD1(coord.y));
+    public static (double x, double y) RoundD1(this (double x, double y) coord) => (Math.RoundD1(coord.x), Math.RoundD1(coord.y));
+    public static (double x, double y) RoundD2(this (double x, double y) coord) => (Math.RoundD2(coord.x), Math.RoundD2(coord.y));
+    public static (double x, double y) RoundD3(this (double x, double y) coord) => (Math.RoundD3(coord.x), Math.RoundD3(coord.y));
+    public static (double x, double y) RoundD4(this (double x, double y) coord) => (Math.RoundD4(coord.x), Math.RoundD4(coord.y));
   }
 }
