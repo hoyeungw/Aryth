4b39c7d8c9fc72841e4d2eea879332d73dafb0f3
hoyeungw <hoyeungw@outlook.com>
2021-08-30 21:37:32 +0800

    fix bugs for RhodoneaFolios, improved accuracy

---
+++ Aryth.Polar/Src/Polar/Graph.Folios.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using static System.Math;
+
+namespace Aryth.Polar {
+  public static class Graph {
+    public const double FoliumAreaOdd = 0.25;
+    public const double FoliumAreaEven = 0.5;
+    public static List<(double r, double θ)> RhodoneaFolios(this List<(double r, double θ)> list,
+                                                            (double r, double θ) rimMark,
+                                                            int petals = 3) {
+      return list.FindAll(polar => polar.r <= rimMark.FoliateRadius(polar.θ, petals));
+    }
+    public static List<(double r, double θ)> RhodoneaFolios(this List<(double r, double θ)> list,
+                                                            (double r, double θ) rimMark,
+                                                            int petals,
+                                                            double density) {
+      var area = PI * Pow(rimMark.r, 2) * (petals % 2 == 0 ? FoliumAreaEven : FoliumAreaOdd);
+      var maximum = (int)Round(density * area);
+      var thresholdPerPhase = maximum / petals;
+      var petalNote = PetalNote.Build(rimMark.θ, petals);
+      var target = new List<(double r, double θ)>(maximum);
+      foreach (var polar in list.FiniteFlopper()) {
+        if (rimMark.FoliateRadius(polar.θ, petals) < polar.r) continue;
+        var phase = petalNote.Phase(polar.θ);
+        if (thresholdPerPhase <= petalNote.Counter[phase]) continue;
+        petalNote.NotePhase(phase);
+        target.Add(polar);
+        if (maximum <= petalNote.Sum) break;
+      }
+      return target;
+    }
+  }
+}
