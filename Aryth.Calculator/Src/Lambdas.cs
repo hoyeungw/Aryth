42353374e81e86d29acf4856ecbdb6f2592c7cf8
hoyeungw <hoyeungw@outlook.com>
2021-10-11 09:29:52 +0800

    add TryParser lambda delegate

---
+++ Aryth.Calculator/Src/Lambdas.cs
@@ -0,0 +1,11 @@
+﻿namespace Aryth {
+  public delegate void ActionByRef<T>(ref T x);
+
+  public delegate bool TryParser<in T, TO>(T x, out TO target);
+
+  public delegate bool TryParser<in TA, in TB, TO>(TA x, TB y, out TO target);
+
+  public delegate void ActionByRef<T1, T2>(ref T1 x, ref T2 y);
+
+  public delegate void IndexedActionByRef<T>(ref T x, int idx);
+}
