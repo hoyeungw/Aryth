b9074cff1fc126f2b5e37fd3954fe79c9786008f
hoyeungw <hoyeungw@outlook.com>
2021-10-11 09:28:59 +0800

    add NaiveOperand, default Operands of double and parser for calculator

---
+++ Aryth.Calculator/Src/Expression/NaiveOperand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Veho;
+using static System.Math;
+
+namespace Aryth.Expression {
+  public static class NaiveOperand {
+    public static readonly Dictionary<string, double> OperandDict = Dict.From(
+      Vec.From(
+        ("PI", PI),
+        ("E", E)
+      ));
+    public static bool TryParseOperand(string text, out dynamic operand) {
+      if (OperandDict.TryGetValue(text, out var result)) {
+        operand = result;
+        return true;
+      }
+      operand = null;
+      return false;
+    }
+  }
+}
