be8d894db18af02be8d546b760a1b6b302bc1bf0
hoyeungw <hoyeungw@outlook.com>
2021-10-11 09:29:31 +0800

    parser for parenthesis and comma

---
+++ Aryth.Calculator/Src/Expression/NaivePlaceholder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Veho;
+
+namespace Aryth.Expression {
+  public static class NaivePlaceholder {
+    public static readonly Dictionary<string, Operator> PlaceholderDict = Dict.From(
+      Vec.From(
+        ("(", Operator.Build("(", Variety.LeftBracket)),
+        (")", Operator.Build(")", Variety.RightBracket)),
+        (",", Operator.Build(",", Variety.Comma))
+      ));
+    public static bool TryParsePlaceholder(string text, out Operator @operator) {
+      return PlaceholderDict.TryGetValue(text, out @operator);
+    }
+  }
+}
