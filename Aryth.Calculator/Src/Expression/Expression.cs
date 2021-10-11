67be165b12108917982c075c123d26f00477b76a
hoyeungw <hoyeungw@outlook.com>
2021-10-11 09:27:51 +0800

    add Expression, basic element for calculator

---
+++ Aryth.Calculator/Src/Expression/Expression.cs
@@ -0,0 +1,7 @@
+﻿namespace Aryth.Expression {
+  public struct Expression : IExpression {
+    public dynamic Value { get; set; }
+    public Variety Variety { get; set; }
+    public override string ToString() => Value.ToString();
+  }
+}
