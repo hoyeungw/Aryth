ff81e100faa304683e4fbe77a48eb3793cd5c17e
hoyeungw <hoyeungw@outlook.com>
2021-10-11 09:30:52 +0800

    add ExpressionQueue

---
+++ Aryth.Calculator/Src/ExpressionQueue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Aryth.Expression;
+
+namespace Aryth {
+  public class ExpressionQueue : Queue<IExpression> {
+    public TryParser<string, dynamic> OperandParser;
+    public TryParser<string, Type[], MethodInfo> AlgebraParser;
+
+    public static ExpressionQueue Build(TryParser<string, dynamic> operandParser = null,
+                                        TryParser<string, Type[], MethodInfo> algebraParser = null) {
+      return new ExpressionQueue {
+                                   OperandParser = operandParser ?? TryParsers.DefaultOperandParser,
+                                   AlgebraParser = algebraParser ?? TryParsers.DefaultMethodParser
+                                 };
+    }
+    public void ParseEnqueue(string text) {
+      if (string.IsNullOrEmpty(text)) return;
+      if (double.TryParse(text, out var number)) Enqueue(number.ToOperand());
+      else if (OperandParser(text, out var value)) this.Enqueue(value.ToOperand());
+      else if (AlgebraParser(text, null, out var method)) Enqueue(method.CastOperator());
+    }
+  }
+}
