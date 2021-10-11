186837d7ddbd88ff027e2b57543871eafbff245d
hoyeungw <hoyeungw@outlook.com>
2021-10-11 09:30:34 +0800

    add Regex for parsing

---
+++ Aryth.Calculator/Src/ExpressionRegex.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Aryth {
+  public static class ExpressionRegex {
+    public const RegexOptions Option = RegexOptions.Compiled | RegexOptions.IgnoreCase;
+    public static readonly Regex Numeric = new Regex(@"[+-]?\d+(?:\.\d+)?", Option);
+    public static readonly Regex Operator = new Regex(@"[+\-*\/^(),]", Option);
+    public static readonly Regex Alphabetic = new Regex(@"[\w\d#$¥%.:|\[\]]+", Option);
+    public static readonly Regex Expression = new Regex($@"({Numeric})|({Operator})|({Alphabetic})", Option);
+  }
+}
