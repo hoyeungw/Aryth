2276f34afbc779cbc730ecc6db2391c8861a57cd
hoyeungw <hoyeungw@outlook.com>
2021-10-29 11:04:24 +0800

    add Restrict and Limit to Math.Double

--- Aryth.Math/Src/Math.Double.cs
+++ Aryth.Math/Src/Math.Double.cs
@@ -1,18 +1,8 @@
 ﻿using static System.Math;
 
 namespace Aryth {
-  public static class Math {
-    public static int IntExp(float x) => x == 0 ? 0 : (int)Log10(x);
+  public static partial class Math {
     public static int IntExp(double x) => x == 0 ? 0 : (int)Log10(x);
-
-    public static float E2 = 100;
-    public static float E3 = 1000;
-    public static float E4 = 10000;
-
-    public static float RoundD1(float x) => (float)Round(x * 10) / 10;
-    public static float RoundD2(float x) => (float)Round(x * E2) / E2;
-    public static float RoundD3(float x) => (float)Round(x * E3) / E3;
-    public static float RoundD4(float x) => (float)Round(x * E4) / E4;
 
     public static double RoundD1(double x) => Round(x * 10) / 10;
     public static double RoundD2(double x) => Round(x * E2) / E2;
@@ -25,9 +15,30 @@ namespace Aryth {
     public static bool HasLOpen(this (double min, double max) interval, double num) => interval.min < num && num <= interval.max;
     public static bool HasROpen(this (double min, double max) interval, double num) => interval.min <= num && num < interval.max;
     public static bool Has(this (double min, double max) interval, double num) => interval.min <= num && num <= interval.max;
-    public static double Restrict(this (double min, double max) interval, double num) {
+    public static double Limit(this (double min, double max) interval, double value) {
       var (min, max) = interval;
-      return num < min ? min : max < num ? max : num;
+      if (value < min) return min;
+      if (value > max) return max;
+      return value;
+    }
+
+    public static double Restrict(this (double min, double max) periodic, double value) {
+      var (min, max) = periodic;
+      var delta = max - min;
+      while (value < min) value += delta;
+      while (value > max) value -= delta;
+      return value;
+    }
+
+    public static double Limit(double value, double max) {
+      if (value < 0) return 0;
+      if (value > max) return max;
+      return value;
+    }
+    public static double Restrict(double value, double max) {
+      while (value < 0) value += max;
+      while (value > max) value -= max;
+      return value;
     }
   }
 }
